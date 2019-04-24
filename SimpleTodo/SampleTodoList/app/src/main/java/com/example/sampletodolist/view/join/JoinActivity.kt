8795302059c4e0023a6e9efef740eb16476b4383
swppppp <sw931110@naver.com>
2019-04-24 16:00:10 +0900

    join 화면 구성

---
+++ SimpleTodo/SampleTodoList/app/src/main/java/com/example/sampletodolist/view/join/JoinActivity.kt
@@ -0,0 +1,23 @@
+package com.example.sampletodolist.view.join
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import com.example.sampletodolist.R
+
+import kotlinx.android.synthetic.main.activity_join.*
+
+class JoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
