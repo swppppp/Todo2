28d51cf3a753a2a447ec89591dda3f471418dca1
swppppp <sw931110@naver.com>
2019-04-25 10:49:26 +0900

    layout 및 value값 세팅

---
+++ SimpleTodo/SampleTodoList/app/src/main/java/com/example/sampletodolist/view/main/MainDrawerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sampletodolist.view.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainDrawerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_drawer)
+    }
+}
